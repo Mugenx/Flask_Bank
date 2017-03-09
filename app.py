98e91ad23941ea8f08d602e73344f587fc82a2aa
Mugenx <sahawu.mail@gmail.com>
2017-03-09 11:19:21 -0500

    Database and fronted update

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, g, request, redirect, url_for, session, flash
-import sqlite3
 from functools import wraps
+import sqlite3
 
 app = Flask(__name__)
 app.secret_key = 'wu'
@@ -13,7 +13,7 @@ def login_required(f):
         if 'login_in' in session:
             return f(*args, **kwargs)
         else:
-            flash('please login first.')
+            flash('Please login first')
             return redirect(url_for('login'))
 
     return wrap
@@ -22,7 +22,7 @@ def login_required(f):
 @app.route('/')
 @login_required
 def index():
-    return "HELLO WORLD!"
+    return redirect(url_for('welcome'))
 
 
 @app.route('/welcome')
@@ -41,7 +41,7 @@ def login():
             session['logged_in'] = True
             flash('Welcome back')
             return redirect(url_for('welcome'))
-    return render_template("login.html", error=error, isin=True)
+    return render_template("login.html", error=error)
 
 
 @app.route('/logout')
@@ -67,47 +67,14 @@ def profile():
 @app.route("/account/<type>")
 def account(type):
     if type == "Chequing":
-        accountNum = 1101
-        balance = 400
+        account_num = 1101
         interest = 0.1
-        transactions = [["Nov 21 2016", "deposit", 50, 600],
-                        ["Jan 2 2017", "Withdraw", 50, 550],
-                        ["Feb 14 2017", "Withdraw", 150, 400]]
+        balance, transactions = get_account_detail(account_num)
     else:
-        accountNum = 2202
-        balance = 100
+        account_num = 2202
         interest = 0.2
-        transactions = [["Nov 26 2016", "deposit", 10, 110],
-                        ["Nov 26 2016", "deposit", 10, 110],
-                        ["Nov 26 2016", "deposit", 10, 110],
-                        ["Nov 26 2016", "deposit", 10, 110],
-                        ["Nov 26 2016", "deposit", 10, 110],
-                        ["Nov 26 2016", "deposit", 10, 110],
-                        ["Nov 26 2016", "deposit", 10, 110],
-                        ["Nov 26 2016", "deposit", 10, 110],
-                        ["Nov 26 2016", "deposit", 10, 110],
-                        ["Nov 26 2016", "deposit", 10, 110],
-                        ["Nov 26 2016", "deposit", 10, 110],
-                        ["Nov 26 2016", "deposit", 10, 110],
-                        ["Nov 26 2016", "deposit", 10, 110],
-                        ["Nov 26 2016", "deposit", 10, 110],
-                        ["Nov 26 2016", "deposit", 10, 110],
-                        ["Nov 26 2016", "deposit", 10, 110],
-                        ["Nov 26 2016", "deposit", 10, 110],
-                        ["Nov 26 2016", "deposit", 10, 110],
-                        ["Nov 26 2016", "deposit", 10, 110],
-                        ["Nov 26 2016", "deposit", 10, 110],
-                        ["Nov 26 2016", "deposit", 10, 110],
-                        ["Nov 26 2016", "deposit", 10, 110],
-                        ["Nov 26 2016", "deposit", 10, 110],
-                        ["Nov 26 2016", "deposit", 10, 110],
-                        ["Nov 26 2016", "deposit", 10, 110],
-                        ["Nov 26 2016", "deposit", 10, 110],
-                        ["Nov 26 2016", "deposit", 10, 110],
-                        ["Nov 26 2016", "deposit", 10, 110],
-                        ["Nov 26 2016", "deposit", 10, 110],
-                        ["Jan 30 2017", "Withdraw", 10, 100]]
-    return render_template("account.html", type=type, accountNum=accountNum, balance=balance,
+        balance, transactions = get_account_detail(account_num)
+    return render_template("account.html", type=type, accountNum=account_num, balance=balance,
                            interest=interest,
                            transactions=transactions)
 
@@ -123,6 +90,18 @@ def db_test():
 
 def connect_db():
     return sqlite3.connect(app.database)
+
+
+def get_account_detail(account_num):
+    g.db = connect_db()
+    cur = g.db.execute(
+        'SELECT Balance FROM transactions WHERE accountNum = %s ORDER BY DATE DESC LIMIT 1' % account_num)
+    balance = [dict(balance=row[0]) for row in cur.fetchall()]
+    cur = g.db.execute('SELECT * FROM transactions WHERE accountNum = %s' % account_num)
+    transactions = [dict(Date=row[1], TransactionDescription=row[2], Action=row[3], Amount=row[4], Balance=row[5])
+                    for row in cur.fetchall()]
+    g.db.close()
+    return balance, transactions
 
 
 if __name__ == "__main__":
--- sql.py
+++ sql.py
@@ -2,7 +2,9 @@ import sqlite3
 
 with sqlite3.connect("db/sample.db") as connection:
     c = connection.cursor()
-    c.execute("DROP TABLE posts")
-    c.execute("CREATE TABLE posts(title TEXT, description TEXT)")
-    c.execute('INSERT INTO posts VALUES("GOOD", "I am good")')
-    c.execute('INSERT INTO posts VALUES("WELL", "I am well")')
+    c.execute("DROP TABLE transactions")
+    c.execute(
+        "CREATE TABLE transactions(accountNum INT, Date DATE, TransactionDescription TEXT, Action TEXT, Amount INT, Balance INT)")
+    c.execute('INSERT INTO transactions VALUES(1101, "2016-11-26", "payroll","deposit",100,100)')
+    c.execute('INSERT INTO transactions VALUES(2202, "2017-02-14", "spend","withdraw",100,100)')
+
