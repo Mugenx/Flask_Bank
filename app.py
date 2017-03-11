dba40a16082da8eba19cf2aa6cf9305b4aaff2fd
Mugenx <sahawu.mail@gmail.com>
2017-03-10 22:29:39 -0500

    user, account databaset created. some pages updated

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from functools import wraps
 import sqlite3
 
 app = Flask(__name__)
-app.secret_key = 'wu'
-app.database = "db/sample.db"
+app.secret_key = 'wu'  # for session
+app.database = "db/sample.db"  # define the database
 
 
 def login_required(f):
@@ -22,25 +22,42 @@ def login_required(f):
 @app.route('/')
 @login_required
 def index():
-    return redirect(url_for('welcome'))
+    return "Flask Bank"
 
 
 @app.route('/welcome')
 def welcome():
-    accountType = ["Chequing", "Saving"]
-    return render_template("welcome.html", accountType=accountType)
+    if session['logged_in'] is True:
+        username = session['username']
+        userid = get_user_id(username)
+        accounts = get_user_accounts(userid)
+        return render_template("welcome.html", username=username, accounts=accounts)
+    else:
+        return redirect(url_for('login'))
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid credentials. Please try again'
+        username = request.form['username']
+        password = request.form['password']
+        if username == '' or password == '':
+            error = 'Username and Password required. Please try again'
         else:
-            session['logged_in'] = True
-            flash('Welcome back')
-            return redirect(url_for('welcome'))
+            g.db = connect_db()
+            cur = g.db.execute('SELECT password FROM users WHERE username = "%s"' % username)
+            pwd = cur.fetchone()[0]
+            g.db.close()
+            print(username)
+            print(pwd)
+            if password != pwd:
+                error = 'Invalid credentials. Please try again'
+            else:
+                session['logged_in'] = True
+                # flash('Welcome to Flask Bank')
+                session['username'] = username
+                return redirect(url_for('welcome'))
     return render_template("login.html", error=error)
 
 
@@ -52,48 +69,85 @@ def logout():
     return redirect(url_for('login'))
 
 
+@app.route('/reg', methods=['POST', 'GET'])
+def reg():
+    if request.method == 'post':
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        firstName = request.form['firstName']
+        lastName = request.form['lastName']
+        phone = request.form['phone']
+        address = request.form['address']
+        city = request.form['city']
+        province = request.form['province']
+        postcode = request.form['postcode']
+
+        with sqlite3.connect(app.database) as con:
+            cur = con.cursor()
+        cur.execute(
+            'INSERT INTO users (username, password, firstName, lastName, email, phone, address,city, province,postcode ) VALUES (?,?,?,?,?,?,?,?,?,?)',
+            (username, password, firstName, lastName, email, phone, address, city, province, postcode))
+        con.commit()
+        con.close()
+    return render_template('reg.html')
+
+
 @app.route("/profile")
 def profile():
-    name = 'wu'
-    info = [
-        ["Name:", name],
-        ["email:", "%s@gmail.com" % name],
-        ["address:", "algonquin college, Woodroffe, Ottawa, ON, CA"],
-        ["phone:", 6130000000]
-    ]
-    return render_template("profile.html", name=name, info=info)
+    username = session['username']
+    results = get_user_detail(username)
+    return render_template("profile.html", results=results, username=username)
 
 
-@app.route("/account/<type>")
-def account(type):
-    if type == "Chequing":
-        account_num = 1101
-        interest = 0.1
-        balance, transactions = get_account_detail(account_num)
-    else:
-        account_num = 2202
-        interest = 0.2
-        balance, transactions = get_account_detail(account_num)
+@app.route("/account/<account_num>")
+def account(account_num):
+    interest = 0.1
+    type, balance, transactions = get_account_detail(account_num)
+    if len(transactions) == 0:
+        transactions = 0
     return render_template("account.html", type=type, accountNum=account_num, balance=balance,
                            interest=interest,
                            transactions=transactions)
 
 
-@app.route('/db')
-def db_test():
-    g.db = connect_db()
-    cur = g.db.execute('SELECT * FROM posts')
-    posts = [dict(title=row[0], description=row[1]) for row in cur.fetchall()]
-    g.db.close()
-    return render_template('db.html', posts=posts)
+@app.route('/transfer')
+def transfer():
+    return render_template('transfer.html')
+
+
+@app.route('/newAccount')
+def new_account():
+    return render_template('newAccount.html')
 
 
 def connect_db():
     return sqlite3.connect(app.database)
 
 
+def get_user_id(username):
+    g.db = connect_db()
+    cur = g.db.execute('SELECT userid FROM users WHERE username = "%s"' % username)
+    userid = cur.fetchone()[0]
+    g.db.close()
+    print(userid)
+    return userid
+
+
+def get_user_accounts(userid):
+    g.db = connect_db()
+    cur = g.db.execute('SELECT * FROM accounts WHERE userid = "%s" ORDER BY type' % userid)
+    accounts = [dict(accountNum=row[1], type=row[2]) for row in cur.fetchall()]
+    g.db.close()
+    print(accounts)
+    return accounts
+
+
 def get_account_detail(account_num):
     g.db = connect_db()
+    cur = g.db.execute(
+        'SELECT type FROM accounts WHERE accountNum = %s' % account_num)
+    type = cur.fetchone()[0]
     cur = g.db.execute(
         'SELECT Balance FROM transactions WHERE accountNum = %s ORDER BY DATE DESC LIMIT 1' % account_num)
     balance = [dict(balance=row[0]) for row in cur.fetchall()]
@@ -101,8 +155,17 @@ def get_account_detail(account_num):
     transactions = [dict(Date=row[1], TransactionDescription=row[2], Action=row[3], Amount=row[4], Balance=row[5])
                     for row in cur.fetchall()]
     g.db.close()
-    return balance, transactions
+    return type, balance, transactions
 
 
+def get_user_detail(username):
+    g.db = connect_db()
+    cur = g.db.execute('SELECT * FROM users WHERE username = "%s"' % username)
+    results = cur.fetchall()
+    g.db.close()
+    return results
+
+
+g
 if __name__ == "__main__":
     app.run(debug=True)
--- sql.py
+++ sql.py
@@ -1,10 +1,14 @@
-import sqlite3
+import sqlite3  # import database module
 
-with sqlite3.connect("db/sample.db") as connection:
-    c = connection.cursor()
-    c.execute("DROP TABLE transactions")
-    c.execute(
-        "CREATE TABLE transactions(accountNum INT, Date DATE, TransactionDescription TEXT, Action TEXT, Amount INT, Balance INT)")
-    c.execute('INSERT INTO transactions VALUES(1101, "2016-11-26", "payroll","deposit",100,100)')
-    c.execute('INSERT INTO transactions VALUES(2202, "2017-02-14", "spend","withdraw",100,100)')
+with sqlite3.connect("db/sample.db") as connection:  # database connection
+    c = connection.cursor()  # define cursor
+    # c.execute("DROP TABLE transactions")  # Drop table
+    # c.execute(  # Create table
+    #     "CREATE TABLE transactions(accountNum INT, Date DATE, TransactionDescription TEXT, Action TEXT, Amount INT, Balance INT)")
+    # c.execute('INSERT INTO transactions VALUES(1101, "2016-11-26", "payroll","deposit",100,100)')  # insert value
+    # c.execute('INSERT INTO transactions VALUES(2202, "2017-02-14", "spend","withdraw",100,100)')
 
+    # c.execute(
+    #     'CREATE TABLE users ( userid INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT, email TEXT )')
+    # c.execute("INSERT INTO users (username,password,email) VALUES (?,?,?)", ('admin', 'admin', 'admin@flaskbank.com'))
+    # c.execute("INSERT INTO users (username,password,email) VALUES (?,?,?)", ('saha', 'saha', 'saha@flaskbank.com'))
